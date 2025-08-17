f82a925d16f988892dbd8b6e5cc5cfc3de36367f
ssidardh <pranaysiddarthjps@gmail.com>
2025-08-17 10:03:35 +0530

    bot working

---
+++ SRC/prompt.py
@@ -0,0 +1,9 @@
+system_prompt=(
+    "you are an assistant for question-answer."
+    "use the following peices of retrieved context to answer "
+    "the question.if you dont know the answer,say that you "
+    "dont know .use three sentences maximum and keep the "
+    "answer concise. "
+    "\n\n"
+    "{context}"
+)
